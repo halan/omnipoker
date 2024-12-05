03f1262933d59dd697998082de98eaf580af285e
Halan Pinheiro <halan.pinheiro@gmail.com>
2024-12-05 12:10:27 -0300

    Improve limit of session

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,12 @@
 use actix_web::{web::Data, App, HttpServer};
 use clap::Parser;
+use limit::Limit;
 use num_cpus;
 use std::sync::{Arc, Mutex};
 
 mod frontend;
 mod game;
+mod limit;
 mod logger;
 mod session;
 mod ws;
@@ -12,7 +14,10 @@ mod ws;
 #[derive(clap::Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
+    #[arg(help = "Specify the address for the server (e.g., '127.0.0.1:8080').")]
     addr: Option<String>,
+    #[arg(short, long, help = "Specify the maximum limit of users.")]
+    limit: Option<usize>,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -21,6 +26,7 @@ async fn main() -> std::io::Result<()> {
 
     let cli = Cli::parse();
     let addr = cli.addr.as_deref().unwrap_or("127.0.0.1:8080");
+    let limit = Limit::new(cli.limit.unwrap_or(15));
 
     log::info!(
         "Starting service: \"planning-poker\", workers: {}, listening on: {}",
@@ -30,7 +36,7 @@ async fn main() -> std::io::Result<()> {
 
     let (mut game_server, game_handler) = game::GameServer::new();
     let server_task = tokio::spawn(async move { game_server.run().await });
-    let session_count = Arc::new(Mutex::new(0usize));
+    let session_count = Arc::new(Mutex::new(limit));
 
     HttpServer::new(move || {
         App::new()
@@ -43,7 +49,7 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await?;
 
-    if let Err(_) = server_task.await {
+    if server_task.await.is_err() {
         log::error!("Game server task failed");
     }
 
--- backend/src/ws.rs
+++ backend/src/ws.rs
@@ -1,4 +1,8 @@
-use crate::{game::GameHandle, session};
+use crate::{
+    game::GameHandle,
+    limit::{release_session, try_acquire_session, Limit},
+    session,
+};
 use actix_web::{
     get,
     web::{self, Payload},
@@ -20,43 +24,12 @@ pub struct QueryParams {
     mode: Option<Mode>,
 }
 
-const MAX_SESSIONS: usize = 15;
-
-fn try_acquire_session(
-    session_count: &web::Data<Arc<Mutex<usize>>>,
-) -> Result<(), actix_web::Error> {
-    let mut session_count_guard = session_count.lock().map_err(|_| {
-        log::error!("Failed to acquire session count lock");
-        actix_web::error::ErrorInternalServerError("Failed to acquire session count lock")
-    })?;
-
-    if *session_count_guard >= MAX_SESSIONS {
-        log::warn!("Too many concurrent sessions; rejecting new session");
-        return Err(actix_web::error::ErrorTooManyRequests(
-            "Too many concurrent sessions",
-        ));
-    }
-
-    *session_count_guard += 1;
-    log::debug!("Session started. Active sessions: {}", *session_count_guard);
-    Ok(())
-}
-
-fn release_session(session_count: &web::Data<Arc<Mutex<usize>>>) {
-    if let Ok(mut session_count_guard) = session_count.lock() {
-        *session_count_guard -= 1;
-        log::debug!("Session ended. Active sessions: {}", *session_count_guard);
-    } else {
-        log::error!("Failed to acquire session count lock for decrement");
-    }
-}
-
 #[get("/ws")]
 pub async fn handler(
     req: HttpRequest,
     stream: Payload,
     game_handler: web::Data<GameHandle>,
-    session_count: web::Data<Arc<Mutex<usize>>>,
+    session_count: web::Data<Arc<Mutex<Limit>>>,
     query: web::Query<QueryParams>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let (res, session, msg_stream) = actix_ws::handle(&req, stream)?;
